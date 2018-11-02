469e513b20c05a41b257a849420fa40fa95fb081
Jhon Chaparro <jhon.chaparro@outlook.com>
2018-11-01 21:12:55 -0500

    Rails

--- railsApp/MyApp/app/models/articulo.rb
+++ railsApp/MyApp/app/models/articulo.rb
@@ -1,4 +1,4 @@
 class Articulo < ApplicationRecord
-  has_many :comentarios
+  has_many :comentarios, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 end
--- railsApp/MyApp/config/routes.rb
+++ railsApp/MyApp/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :articulos
+  devise_for :users
+  resources :articulos do
+    resources :comentarios
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'bienvenido#index'
 end
